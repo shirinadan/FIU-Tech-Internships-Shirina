f5a49f739752bcaecbd9a71be4da77500d34bc21
Shirina Shaji Daniel <slnu006@fiu.edu>
2026-07-20 16:16:30 -0400

    update fiu logo

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -98,7 +98,7 @@ const Index = () => {
       aria-label="Go to homepage"
     >
       <img
-        src="FIU-Tech-Internships-Shirina\images\fiu-kfscis-logo.svg"
+        src="/images/fiu-kfscis-logo.png" 
         alt="FIU KFSCIS logo"
         className="h-14 sm:h-16 md:h-20 w-auto object-contain shrink-0"
         style={{ filter: `drop-shadow(0 0 14px ${FIU.brightGold}44)` }}
