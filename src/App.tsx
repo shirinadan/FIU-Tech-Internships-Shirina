d48fa2484031b84c8c6763054080a9f5476056f7
Shirina Shaji Daniel <slnu006@fiu.edu>
2026-07-20 14:46:53 -0400

    Update app.tsx, index.css, index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import Index from "./pages/Index"; //import the page
 import NotFound from "./pages/NotFound";
+
 
 const queryClient = new QueryClient();
 
@@ -19,7 +21,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
-    </TooltipProvider>
+    </TooltipProvider>Content cards
   </QueryClientProvider>
 );
 
