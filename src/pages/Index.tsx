60cf68107421cd4c911f991a4a801c261a18b8a6
Shirina Shaji Daniel <slnu006@fiu.edu>
2026-07-20 16:31:25 -0400

    update fiu logo

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -98,7 +98,7 @@ const Index = () => {
       aria-label="Go to homepage"
     >
       <img
-        src={fiuLogo} 
+        src={fiu-kfscis-logo} 
         alt="FIU KFSCIS logo"
         className="h-14 sm:h-16 md:h-20 w-auto object-contain shrink-0"
         style={{ filter: `drop-shadow(0 0 14px ${FIU.brightGold}44)` }}
